12560140678cdc82ce6826d8d8efb292ce1545eb
ckaotik <ckaotik@gmail.com>
2014-09-30 10:05:43 +0200

    Some code cleanups.

--- tooltip/glyph.lua
+++ tooltip/glyph.lua
@@ -1,10 +1,9 @@
 local addonName, ns, _ = ...
--- FIXME: Several glyphs have the save name but are for different classes (e.g. Stampede)
--- local characters = ns.data.GetCharacters()
 
--- ================================================
---  Glyphs
--- ================================================
+-- FIXME: Several glyphs have the save name but are for different classes (e.g. Stampede)
+-- GLOBALS: _G, DataStore, wipe, ipairs
+local tinsert, tsort, tconcat = table.insert, table.sort, table.concat
+
 local glyphKnown = {}
 local glyphUnknown = {}
 local function GetGlyphKnownInfo(glyph, onlyUnknown)
@@ -14,14 +13,14 @@ local function GetGlyphKnownInfo(glyph, onlyUnknown)
 	for _, character in ipairs(ns.data.GetCharacters()) do
 		local isKnown, canLearn = DataStore:IsGlyphKnown(character, glyph)
 		if isKnown and not onlyUnknown then
-			table.insert(glyphKnown, ns.data.GetCharacterText(character))
+			tinsert(glyphKnown, ns.data.GetCharacterText(character))
 		elseif canLearn then
-			table.insert(glyphUnknown, ns.data.GetCharacterText(character))
+			tinsert(glyphUnknown, ns.data.GetCharacterText(character))
 		end
 	end
-	-- glyphs only known/lernable for 1 class, so wie can sort by |cxxxxxxxxName|r
-	table.sort(glyphKnown)
-	table.sort(glyphUnknown)
+	-- glyphs only known/learnable for 1 class, so we can sort by |cxxxxxxxxName|r
+	tsort(glyphKnown)
+	tsort(glyphUnknown)
 	return glyphKnown, glyphUnknown
 end
 
@@ -31,14 +30,14 @@ function ns.AddGlyphInfo(tooltip, glyph)
 	local linesAdded = false
 	local known, unknown = GetGlyphKnownInfo(glyph, onlyUnknown)
 
-	local list = not onlyUnknown and table.concat(known, ", ")
+	local list = not onlyUnknown and tconcat(known, ", ")
 	if list and list ~= "" then
-		tooltip:AddLine(RED_FONT_COLOR_CODE..ITEM_SPELL_KNOWN..": "..FONT_COLOR_CODE_CLOSE..list, nil, nil, nil, true)
+		tooltip:AddLine(_G.RED_FONT_COLOR_CODE.._G.ITEM_SPELL_KNOWN..": ".._G.FONT_COLOR_CODE_CLOSE..list, nil, nil, nil, true)
 		linesAdded = true
 	end
-	list = table.concat(unknown, ", ")
+	list = tconcat(unknown, ", ")
 	if list and list ~= "" then
-		tooltip:AddLine(GREEN_FONT_COLOR_CODE..UNKNOWN..": "..FONT_COLOR_CODE_CLOSE..list, nil, nil, nil, true)
+		tooltip:AddLine(_G.GREEN_FONT_COLOR_CODE.._G.UNKNOWN..": ".._G.FONT_COLOR_CODE_CLOSE..list, nil, nil, nil, true)
 		linesAdded = true
 	end
 	return linesAdded
