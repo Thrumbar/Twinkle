f153e122bec2cf0b3c9d917f2fd053b160ec6d7d
ckaotik <ckaotik@gmail.com>
2014-11-13 13:45:48 +0100

    Brokers: Lockouts: Fixed tooltip not displaying any info. Currently only displays SoO. Added line highlight and lockout tooltip on hover.

--- brokers/lockouts.lua
+++ brokers/lockouts.lua
@@ -8,44 +8,18 @@ local addonName, addon, _ = ...
 local brokers = addon:GetModule('brokers')
 local broker = brokers:NewModule('Lockouts')
 
-local instanceLockouts = {}
-local function GetCharacterInstanceLockouts(characterKey)
-	local hasData = nil
-	wipe(instanceLockouts)
-
-	-- DataStore:IsEncounterDefeated()
-	for lockoutID, lockoutLink in DataStore:IterateInstanceLockouts(characterKey) do
-		hasData = true
-		local instanceName, difficulty, reset, isExtended, isRaid, numDefeated, numBosses = DataStore:GetInstanceLockoutInfo(characterKey, lockoutID)
-		local color = (reset == 0 and _G.GRAY_FONT_COLOR_CODE)
-			or (numDefeated > 0 and _G.GREEN_FONT_COLOR_CODE)
-			or _G.NORMAL_FONT_COLOR_CODE
-		local rowText = string.format('%s%d/%d|r', color, numDefeated, numBosses)
-		table.insert(instanceLockouts, rowText)
-	end
-	return hasData and instanceLockouts or nil
-end
-
-local function GetFlexLockouts(characterKey)
-	local numDefeated, numBosses = 0, 0
-	for instanceID, instanceName, status, reset, defeated, bosses in DataStore:IterateLFGs(characterKey, _G.TYPEID_DUNGEON, _G.LFG_SUBTYPEID_FLEXRAID) do
-		numDefeated = numDefeated + (defeated or 0)
-		numBosses   = numBosses + (bosses or 0)
-	end
-	return numDefeated, numBosses
-end
-
 local function GetInstanceLockouts(characterKey, difficulty, instanceName)
-	local numDefeated, numBosses, hasID = 0, 0, nil
+	local numDefeated, numBosses, hasID, link = 0, 0, nil, nil
 	for lockoutID, lockoutLink in DataStore:IterateInstanceLockouts(characterKey) do
 		local name, diff, reset, _, _, defeated, bosses = DataStore:GetInstanceLockoutInfo(characterKey, lockoutID)
 		if diff == difficulty and name == instanceName then
 			numDefeated = numDefeated + defeated
 			numBosses   = numBosses + bosses
 			hasID       = reset ~= 0
+			link        = lockoutLink
 		end
 	end
-	return numDefeated, numBosses, hasID
+	return numDefeated, numBosses, hasID, link
 end
 
 function broker:OnEnable()
@@ -65,60 +39,62 @@ function broker:UpdateLDB()
 end
 
 local function NOOP() end -- do nothing
+local instanceLinks = {}
 function broker:UpdateTooltip()
 	local numColumns = 4
 	self:SetColumnLayout(numColumns, 'LEFT', 'RIGHT')
 
 	local lineNum = self:AddHeader()
 	self:SetCell(lineNum, 1, addonName .. ': ' .. _G.RAID, 'LEFT', numColumns)
-	-- self:AddSeparator(2)
-
-	-- _G.ERR_LOOT_GONE
-	--[[ for _, characterKey in ipairs(brokers:GetCharacters()) do
-		local data = GetCharacterInstanceLockouts(characterKey, true)
-		if data then
-			lineNum = self:AddLine(addon.data.GetCharacterText(characterKey),  unpack(data))
-			self:SetLineScript(lineNum, 'OnEnter', NOOP) -- show highlight on row
-		end
-	end --]]
-
 	self:AddSeparator(2)
-	lineNum = self:AddHeader('Character', 'Flex', 'Normal', 'Heroic')
-	lineNum = self:AddLine()
+
+	-- GetNumRFDungeons(), GetRFDungeonInfo(index)
+	-- GetNumFlexRaidDungeons(), GetFlexRaidDungeonInfo(index)
+
+	lineNum = self:AddHeader('Character', 'Normal', 'Heroic', 'Mythic')
 	for _, characterKey in ipairs(brokers:GetCharacters()) do
-		self:SetCell(lineNum, 1, addon.data.GetCharacterText(characterKey))
+		local instanceName = 'Schlacht um Orgrimmar'
+		local statusFormat, totalDefeated = '%s%d/%d|r', 0
+		local numDefeated, numBosses, hasID, color, instanceLink
+		wipe(instanceLinks)
 
-		-- FIXME: MoP only has one flex instance group
-		local flexDefeated, flexBosses = GetFlexLockouts(characterKey)
-		local color = (flexDefeated > 0 and _G.GREEN_FONT_COLOR_CODE) or _G.NORMAL_FONT_COLOR_CODE
-		self:SetCell(lineNum, 2, string.format('%s%d/%d|r', color, flexDefeated, flexBosses))
+		-- FIXME: chinese have separate lockous for 10/25
+		numDefeated, numBosses, hasID, instanceLink = GetInstanceLockouts(characterKey, 14, instanceName)
+		color = (not hasID and numDefeated > 0 and _G.GRAY_FONT_COLOR_CODE)
+			or (numDefeated > 0 and _G.GREEN_FONT_COLOR_CODE)
+			or _G.NORMAL_FONT_COLOR_CODE
+		local nhc = string.format(statusFormat, color, numDefeated, numBosses)
+		table.insert(instanceLinks, instanceLink or '')
+		totalDefeated = totalDefeated + numDefeated
 
-		-- FIXME: chinese have separate lockous
-		local defeated10, defeated25, hasID
-		defeated10, nBosses, locked = GetInstanceLockouts(characterKey, 3, 'Schlacht um Orgrimmar') -- 10 normal
-		hasID = hasID or locked
-		defeated25, _, locked = GetInstanceLockouts(characterKey, 4, 'Schlacht um Orgrimmar') -- 25 normal
-		hasID = hasID or locked
-		local nDefeated = math.max(defeated10, defeated25)
-		local color = (not hasID and nDefeated > 0 and _G.GRAY_FONT_COLOR_CODE) or (nDefeated > 0 and _G.GREEN_FONT_COLOR_CODE) or _G.NORMAL_FONT_COLOR_CODE
-		self:SetCell(lineNum, 3, string.format('%s%d/%d|r', color, nDefeated, nBosses))
+		numDefeated, numBosses, hasID, instanceLink = GetInstanceLockouts(characterKey, 15, instanceName)
+		color = (not hasID and numDefeated > 0 and _G.GRAY_FONT_COLOR_CODE)
+			or (numDefeated > 0 and _G.GREEN_FONT_COLOR_CODE)
+			or _G.NORMAL_FONT_COLOR_CODE
+		local hc = string.format(statusFormat, color, numDefeated, numBosses)
+		table.insert(instanceLinks, instanceLink or '')
+		totalDefeated = totalDefeated + numDefeated
 
-		hasID = false
-		defeated10, hcBosses, locked = GetInstanceLockouts(characterKey, 5, 'Schlacht um Orgrimmar') -- 10 heroic
-		hasID = hasID or locked
-		defeated25, _, locked = GetInstanceLockouts(characterKey, 6, 'Schlacht um Orgrimmar') -- 25 heroic
-		hasID = hasID or locked
-		local hcDefeated = math.max(defeated10, defeated25)
-		local color = (not hasID and hcDefeated and _G.GRAY_FONT_COLOR_CODE) or (hcDefeated > 0 and _G.GREEN_FONT_COLOR_CODE) or _G.NORMAL_FONT_COLOR_CODE
-		self:SetCell(lineNum, 4, string.format('%s%d/%d|r', color, hcDefeated, hcBosses))
+		numDefeated, numBosses, hasID, instanceLink = GetInstanceLockouts(characterKey, 16, instanceName)
+		color = (not hasID and numDefeated > 0 and _G.GRAY_FONT_COLOR_CODE)
+			or (numDefeated > 0 and _G.GREEN_FONT_COLOR_CODE)
+			or _G.NORMAL_FONT_COLOR_CODE
+		local mc = string.format(statusFormat, color, numDefeated, numBosses)
+		table.insert(instanceLinks, instanceLink or '')
+		totalDefeated = totalDefeated + numDefeated
 
-		if flexDefeated ~= 0 or nDefeated ~= 0 or hcDefeated ~= 0 then
-			-- when adding a character line, create new followup
-			lineNum = self:AddLine()
+		if totalDefeated > 0 then
+			lineNum = self:AddLine(addon.data.GetCharacterText(characterKey), nhc, hc, mc)
+			self:SetLineScript(lineNum, 'OnEnter', NOOP) -- show highlight on row
+			for column = 2, numColumns do
+				local instanceLink = instanceLinks[column - 1]
+				if instanceLink ~= '' then
+					local cell = self.lines[lineNum].cells[column]
+					      cell.link = instanceLink
+					self:SetCellScript(lineNum, column, 'OnEnter', addon.ShowTooltip, self)
+					self:SetCellScript(lineNum, column, 'OnLeave', addon.HideTooltip, self)
+				end
+			end
 		end
-	end
-	-- remove excess line
-	for column = 1, numColumns do
-		self:SetCell(lineNum, column, nil)
 	end
 end
