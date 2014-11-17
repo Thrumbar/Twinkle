2d7230cfd09bacef8ba215b3cd4472c0792d6345
ckaotik <ckaotik@gmail.com>
2014-11-17 13:41:26 +0100

    Added Draenor currency ids

--- views/lists.currencies.lua
+++ views/lists.currencies.lua
@@ -12,38 +12,51 @@ local currencies = lists:NewModule('Currencies', 'AceEvent-3.0')
 
 -- copied from wowhead.com/currencies
 local currencyIDs = {
-	 61, -- Dalaran Jewelcrafter's Token
-	 81, -- Epicurean's Award
-	241, -- Champion's Seal
-	361, -- Illustrious Jewelcrafter's Token
-	384, -- Dwarf Archaeology Fragment
-	385, -- Troll Archaeology Fragment
-	390, -- Conquest Points
-	391, -- Tol Barad Commendation
-	392, -- Honor Points
-	393, -- Fossil Archaeology Fragment
-	394, -- Night Elf Archaeology Fragment
-	395, -- Justice Points
-	396, -- Valor Points
-	397, -- Orc Archaeology Fragment
-	398, -- Draenei Archaeology Fragment
-	399, -- Vrykul Archaeology Fragment
-	400, -- Nerubian Archaeology Fragment
-	401, -- Tol'vir Archaeology Fragment
-	402, -- Ironpaw Token
-	416, -- Mark of the World Tree
-	515, -- Darkmoon Prize Ticket
-	614, -- Mote of Darkness
-	615, -- Essence of Corrupted Deathwing
-	676, -- Pandaren Archaeology Fragment
-	677, -- Mogu Archaeology Fragment
-	697, -- Elder Charm of Good Fortune
-	738, -- Lesser Charm of Good Fortune
-	752, -- Mogu Rune of Fate
-	754, -- Mantid Archaeology Fragment
-	776, -- Warforged Seal
-	777, -- Timeless Coin
-	789, -- Bloody Coin
+	  61, -- Dalaran Jewelcrafter's Token
+	  81, -- Epicurean's Award
+	 241, -- Champion's Seal
+	 361, -- Illustrious Jewelcrafter's Token
+	 384, -- Dwarf Archaeology Fragment
+	 385, -- Troll Archaeology Fragment
+	 390, -- Conquest Points
+	 391, -- Tol Barad Commendation
+	 392, -- Honor Points
+	 393, -- Fossil Archaeology Fragment
+	 394, -- Night Elf Archaeology Fragment
+	 395, -- Justice Points
+	 396, -- Valor Points
+	 397, -- Orc Archaeology Fragment
+	 398, -- Draenei Archaeology Fragment
+	 399, -- Vrykul Archaeology Fragment
+	 400, -- Nerubian Archaeology Fragment
+	 401, -- Tol'vir Archaeology Fragment
+	 402, -- Ironpaw Token
+	 416, -- Mark of the World Tree
+	 515, -- Darkmoon Prize Ticket
+	 614, -- Mote of Darkness
+	 615, -- Essence of Corrupted Deathwing
+	 676, -- Pandaren Archaeology Fragment
+	 677, -- Mogu Archaeology Fragment
+	 697, -- Elder Charm of Good Fortune
+	 738, -- Lesser Charm of Good Fortune
+	 752, -- Mogu Rune of Fate
+	 754, -- Mantid Archaeology Fragment
+	 776, -- Warforged Seal
+	 777, -- Timeless Coin
+	 789, -- Bloody Coin
+	 821, -- Draenor Clans Archaeology Fragment
+	 823, -- Apexis Crystal
+	 824, -- Garrison Resources
+	 828, -- Ogre Archaeology Fragment
+	 829, -- Arakkoa Archaeology Fragment
+	 910, -- Secret of Draenor Alchemy
+	 944, -- Artifact Fragment
+	 980, -- Dingy Iron Coins
+	 994, -- Seal of Tempered Fate
+	 999, -- Secret of Draenor Tailoring
+	1008, -- Secret of Draenor Jewelcrafting
+	1017, -- Secret of Draenor Leatherworking
+	1020, -- Secret of Draenor Blacksmithing
 }
 
 function currencies:OnEnable()
