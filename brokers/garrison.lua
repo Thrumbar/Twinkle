0c3b440ef3556a94f495635997152d118413fe56
ckaotik <ckaotik@gmail.com>
2015-06-01 13:25:00 +0200

    Brokers: Garrison: Now also displaying resources. Now displays when a buildings' last work order finishes

--- brokers/garrison.lua
+++ brokers/garrison.lua
@@ -22,24 +22,27 @@ function broker:UpdateLDB()
 	self.icon = 'Interface\\FriendsFrame\\UI-Toast-BroadcastIcon'
 end
 
-local COMPLETE = _G.GREEN_FONT_COLOR_CODE .. '%1$d|r'
-local COMPLETE_ACTIVE = _G.GREEN_FONT_COLOR_CODE .. '%1$d|r/'.._G.NORMAL_FONT_COLOR_CODE .. '%2$d|r'
-local COMPLETE_ACTIVE_INACTIVE = _G.GREEN_FONT_COLOR_CODE .. '%1$d|r/'.._G.NORMAL_FONT_COLOR_CODE .. '%2$d|r/'.._G.RED_FONT_COLOR_CODE .. '%3$d|r'
-local INACTIVE = _G.GRAY_FONT_COLOR_CODE .. '%3$d|r'
+local COMPLETE = _G.GREEN_FONT_COLOR_CODE .. '%1$s|r'
+local COMPLETE_ACTIVE = _G.GREEN_FONT_COLOR_CODE .. '%1$s|r/'.._G.NORMAL_FONT_COLOR_CODE .. '%2$s|r'
+local INACTIVE = _G.GRAY_FONT_COLOR_CODE .. '%3$s|r'
 local ZERO = _G.GRAY_FONT_COLOR_CODE .. '0|r'
 function broker:UpdateTooltip()
-	local numColumns = 4
-	self:SetColumnLayout(numColumns, 'LEFT', 'CENTER', 'CENTER', 'LEFT')
+	local numColumns = 6
+	self:SetColumnLayout(numColumns, 'LEFT', 'RIGHT', 'RIGHT', 'CENTER', 'CENTER', 'LEFT')
 
 	local lineNum = self:AddHeader()
 	self:SetCell(lineNum, 1, addonName .. ': Garrisons', 'LEFT', numColumns)
 
-	lineNum = self:AddHeader(_G.CHARACTER, 'Builds', 'Missions', 'Work Orders')
+	-- lineNum = self:AddHeader(_G.CHARACTER, 'Resources', 'Builds', 'Missions', 'Work Orders')
+	local _, _, resourceIcon = GetCurrencyInfo(824)
+	            resourceIcon = '|T'..resourceIcon..':0|t'
+	local missionIcon = '|TInterface\\HELPFRAME\\OpenTicketIcon:18|t'
+	local buildIcon = '|TInterface\\WorldStateFrame\\NeutralTower:0:0:0:0:32:32:3:19:2:18|t'
+	lineNum = self:AddHeader(_G.CHARACTER, resourceIcon, '|TInterface\\FriendsFrame\\StatusIcon-Away:0|t', buildIcon, missionIcon, 'Work Orders')
 	self:AddSeparator(2)
 
-	-- character name, completed/active builds, completed/active missions, completed/active/inactive work orders
-
 	local now, hasData = time(), false
+	local shipmentInterval = 4*60*60 -- every four hours
 	for _, characterKey in ipairs(brokers:GetCharacters()) do
 		-- shipments
 		local shipments = ''
@@ -49,11 +52,20 @@ function broker:UpdateTooltip()
 				-- additional sets that have been completed
 				active = active - 1
 				completed = (completed or 0) + 1
-				nextBatch = nextBatch + 4*60*60
+				nextBatch = nextBatch + shipmentInterval
+			end
+			local fulfilled = active > 0 and (nextBatch + (active-1) * shipmentInterval) or nil
+			      fulfilled = fulfilled and fulfilled - time()
+			if fulfilled then
+				local textFormat, value = SecondsToTimeAbbrev(fulfilled)
+				fulfilled = '|TInterface\\FriendsFrame\\StatusIcon-Away:0|t' .. (fulfilled < shipmentInterval and _G.RED_FONT_COLOR_CODE or _G.NORMAL_FONT_COLOR_CODE) .. textFormat:format(value) .. '|r'
 			end
 
 			local _, _, _, icon = C_Garrison.GetBuildingInfo(buildingID)
-			shipments = (shipments ~= '' and shipments..' ' or '') .. '|T'..icon..':0|t ' .. (active + completed > 0 and COMPLETE_ACTIVE_INACTIVE or INACTIVE):format(completed, active, max - active - completed)
+			shipments = (shipments ~= '' and shipments..' ' or '')
+				.. '|T'..icon..':0|t '
+				.. (completed > 0 and COMPLETE:format(completed)..' ' or '')
+				.. (fulfilled or INACTIVE:format(nil, nil, max))
 		end
 
 		-- builds
@@ -80,12 +92,25 @@ function broker:UpdateTooltip()
 		end
 		local missions = (numActive > 0 or numCompleted > 0) and COMPLETE_ACTIVE:format(numCompleted, numActive) or ZERO
 
-		if builds ~= '' or missions ~= '' or shipments ~= '' then
+		-- resources
+		local _, _, numResources, _, numUncollectedResources = addon.data.GetCurrencyInfo(characterKey, 824)
+
+		-- create tooltip row
+		if builds ~= '' or missions ~= '' or shipments ~= ''
+			or numResources > 0 or numUncollectedResources > 0 then
 			local characterName = addon.data.GetCharacterText(characterKey)
-			lineNum = self:AddLine(characterName, builds, missions, shipments .. '\32\32')
-			          self:SetLineScript(lineNum, 'OnEnter', nop) -- show highlight on row
+			lineNum = self:AddLine(characterName,
+				addon.ColorizeText(AbbreviateLargeNumbers(numResources), 10000 - numResources, 10000),
+				addon.ColorizeText(numUncollectedResources, 500 - numUncollectedResources, 500),
+				builds,
+				missions,
+				shipments .. '\32\32'
+			)
+			-- enable row highlight
+			self:SetLineScript(lineNum, 'OnEnter', nop)
 			hasData = true
 		end
 	end
+
 	return not hasData
 end
