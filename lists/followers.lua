ef2090bc5411a1e201a7e0881eae2ebbca01f795
ckaotik <ckaotik@gmail.com>
2015-06-17 21:27:31 +0200

    Lists: Garrison Followers: Swapped abilities and traits

--- lists/followers.lua
+++ lists/followers.lua
@@ -13,7 +13,13 @@ local plugin   = lists:NewModule('Followers', 'AceEvent-3.0')
       plugin.title = _G.GARRISON_FOLLOWERS_TITLE
       plugin.excludeItemSearch = true
 
+local mechanics = {}
 function plugin:OnEnable()
+	for _, mechanic in pairs(C_Garrison.GetAllEncounterThreats()) do
+		-- table.insert(mechanics, mechanic.id)
+		mechanics[mechanic.id] = true
+	end
+
 	-- self:RegisterEvent('USE_GLYPH', 'Update')
 end
 function plugin:OnDisable()
@@ -72,10 +78,16 @@ function plugin:GetRowInfo(characterKey, index)
 	local labelFormat, traitFormat = '%3$s%2$s|r %1$s', '%2$s|T:1:1|t|T%1$s:15|t'
 	local color = RGBTableToColorCode(_G.ITEM_QUALITY_COLORS[quality])
 	local label = labelFormat:format(name, (iLevel and iLevel > 600) and iLevel or level, color)
-	if trait1 > 0 then suffix = traitFormat:format(C_Garrison.GetFollowerAbilityIcon(trait1), suffix) end
-	if trait2 > 0 then suffix = traitFormat:format(C_Garrison.GetFollowerAbilityIcon(trait2), suffix) end
-	if trait3 > 0 then suffix = traitFormat:format(C_Garrison.GetFollowerAbilityIcon(trait3), suffix) end
-	if trait4 > 0 then suffix = traitFormat:format(C_Garrison.GetFollowerAbilityIcon(trait4), suffix) end
+	for i = 1, 4 + 4 do
+		local abilityID = select(3 + i, DataStore:GetFollowerInfo(characterKey, garrFollowerID))
+		if abilityID > 0 then
+			local mechanicID, _, icon = C_Garrison.GetFollowerAbilityCounterMechanicInfo(abilityID)
+			if icon and mechanics[mechanicID] then
+				-- most traits don't counter any mechanics
+				suffix = traitFormat:format(icon, suffix)
+			end
+		end
+	end
 
 	return nil, label, prefix, suffix, link
 end
@@ -85,15 +97,12 @@ function plugin:GetItemInfo(characterKey, index, itemIndex)
 	if not garrFollowerID or itemIndex > 4 then return end
 
 	local icon, link, tooltipText, count
-	local abilityID = select(3 + itemIndex, DataStore:GetFollowerInfo(characterKey, garrFollowerID))
+	local abilityID = select(3 + 4 + itemIndex, DataStore:GetFollowerInfo(characterKey, garrFollowerID))
 	if abilityID and abilityID > 0 then
-		local _, counters, counterIcon = C_Garrison.GetFollowerAbilityCounterMechanicInfo(abilityID)
+		-- link = C_Garrison.GetFollowerAbilityLink(abilityID)
 		icon = C_Garrison.GetFollowerAbilityIcon(abilityID)
 		tooltipText = '|T' .. icon .. ':0|t ' .. C_Garrison.GetFollowerAbilityName(abilityID)
 			.. '|n' .. C_Garrison.GetFollowerAbilityDescription(abilityID)
-		--	.. '|n' .. (_G.GARRISON_ABILITY_COUNTERS .. ' |T%s:0|t %s'):format(counterIcon, counters)
-		-- link = C_Garrison.GetFollowerAbilityLink(abilityID)
-		icon  = counterIcon
 		count = 1
 	end
 	return icon, link, tooltipText, count
