88aefc54822604ef72c6efb6a72b7ecd538da1fd
ckaotik <ckaotik@gmail.com>
2016-07-23 13:49:26 +0200

    Removed unused function

--- modules/tooltip.lua
+++ modules/tooltip.lua
@@ -120,10 +120,6 @@ local function HandleTooltipItem(self, link)
 	self:Show()
 end
 
---[[ local function HandleTradeSkillResult(self, recipeID)
-	local itemLink = C_TradeSkillUI.GetRecipeItemLink(recipeID)
-	HandleTooltipItem(self, itemLink)
-end --]]
 local function HandleTradeSkillReagent(self, recipeID, reagentIndex)
 	local itemLink = C_TradeSkillUI.GetRecipeReagentItemLink(recipeID, reagentIndex)
 	HandleTooltipItem(self, itemLink)
@@ -233,7 +229,6 @@ local function HookTooltip(tooltip)
 	end
 	-- tooltip:HookScript('OnTooltipSetEquipmentSet', function(self) end) -- ??
 
-	-- if tooltip.SetRecipeResultItem then hooksecurefunc(tooltip, 'SetRecipeResultItem', HandleTradeSkillResult) end
 	if tooltip.SetRecipeReagentItem then hooksecurefunc(tooltip, 'SetRecipeReagentItem', HandleTradeSkillReagent) end
 	if tooltip.SetHyperlink then hooksecurefunc(tooltip, 'SetHyperlink', HandleTooltipHyperlink) end
 	-- if tooltip.SetItemByID then hooksecurefunc(tooltip, 'SetItemByID', HandleTooltipItem) end
