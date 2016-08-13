7b3146a767f23bfc489ad7814efa0f9157e7c286
ckaotik <ckaotik@gmail.com>
2016-08-13 12:27:03 +0200

    Updated config

--- config.lua
+++ config.lua
@@ -1,77 +1,38 @@
 local addonName, addon, _ = ...
+local AceConfigDialog = LibStub('AceConfigDialog-3.0')
 
-local function GetCurrencyLabel(currencyID, value)
-	local name, _, texture, _, weeklyMax, totalMax, isDiscovered = GetCurrencyInfo(currencyID)
-	local label = name or currencyID
-	if name then
-		label = ('|T%2$s:0|t %3$s%1$s|r'):format(name, texture, isDiscovered and _G.NORMAL_FONT_COLOR_CODE or _G.GRAY_FONT_COLOR_CODE)
-	end
-	return currencyID, label
-end
-
-local function GetReminderLabel(interval, value)
-	-- alternative: D_MINUTES
-	return interval, _G.PET_TIME_LEFT_MINUTES:format(interval)
-end
-
-local function GetFactionIcons()
-	local icons = {
-		'Interface\\FriendsFrame\\PlusManz-%s',
-		'Interface\\GROUPFRAME\\UI-Group-PVP-%s',
-		'Interface\\PVPFrame\\PVPCurrency-Conquest-%s',
-		'Interface\\PVPFrame\\PVPCurrency-Honor-%s',
-		'Interface\\WorldStateFrame\\%sIcon',
-		'Interface\\TARGETINGFRAME\\UI-PVP-%s',
-		'Interface\\Timer\\%s-Logo',
-	}
-	local options = {
-		[''] = _G.NONE,
-	}
-	for _, icon in ipairs(icons) do
-		options[icon] = '|T'..icon:format('Horde')..':20|t |T'..icon:format('Alliance')..':20|t'
-	end
-	return options
-end
-
-local characters = {}
-local function GetDeletableCharacters()
-	local thisCharacter = addon.data.GetCurrentCharacter()
-	addon.data.GetAllCharacters(characters)
-	for i, characterKey in ipairs(characters) do
-		if characterKey ~= thisCharacter then
-			characters[characterKey] = ('%s: %s'):format(addon.data.GetRealm(characterKey), addon.data.GetCharacterText(characterKey))
-			-- local realmName = addon.data.GetRealm(characterKey)
-			-- characters[realmName] = characters[realmName] or {}
-			-- characters[realmName][characterKey] = addon.data.GetCharacterText(characterKey)
+local function GetConfigurationVariables()
+	local function GetCurrencyLabel(currencyID, value)
+		local name, _, texture, _, weeklyMax, totalMax, isDiscovered = GetCurrencyInfo(currencyID)
+		local label = name or currencyID
+		if name then
+			label = ('|T%2$s:0|t %3$s%1$s|r'):format(name, texture, isDiscovered and _G.NORMAL_FONT_COLOR_CODE or _G.GRAY_FONT_COLOR_CODE)
 		end
-		characters[i] = nil
+		return currencyID, label
 	end
-	return characters
-end
-
-StaticPopupDialogs['TWINKLE_DELETE_CHARACTER'] = {
-  text = 'Are you sure you want to delete |nall data of %s on %s?',
-  button1 = _G.OKAY,
-  button2 = _G.CANCEL,
-  OnAccept = function(_, characterKey) addon.data.DeleteCharacter(characterKey) end,
-  timeout = 0,
-  whileDead = true,
-  hideOnEscape = true,
-  preferredIndex = 3,
-}
-
-local function OpenConfiguration(self, args)
-	-- remove placeholder configuration panel
-	for i, panel in ipairs(_G.INTERFACEOPTIONS_ADDONCATEGORIES) do
-		if panel == self then
-			tremove(INTERFACEOPTIONS_ADDONCATEGORIES, i)
-			break
+	local function GetReminderLabel(interval, value)
+		-- alternative: D_MINUTES
+		return interval, _G.PET_TIME_LEFT_MINUTES:format(interval)
+	end
+	local function GetFactionIcons()
+		local icons = {
+			'Interface\\FriendsFrame\\PlusManz-%s',
+			'Interface\\GROUPFRAME\\UI-Group-PVP-%s',
+			'Interface\\PVPFrame\\PVPCurrency-Conquest-%s',
+			'Interface\\PVPFrame\\PVPCurrency-Honor-%s',
+			'Interface\\WorldStateFrame\\%sIcon',
+			'Interface\\TARGETINGFRAME\\UI-PVP-%s',
+			'Interface\\Timer\\%s-Logo',
+		}
+		local options = {
+			[''] = _G.NONE,
+		}
+		for _, icon in ipairs(icons) do
+			options[icon] = '|T'..icon:format('Horde')..':20|t |T'..icon:format('Alliance')..':20|t'
 		end
+		return options
 	end
-	self:SetScript('OnShow', nil)
-	self:Hide()
 
-	-- initialize panel
 	local types = {
 		-- main settings
 		factionIcon = GetFactionIcons,
@@ -114,7 +75,7 @@ local function OpenConfiguration(self, args)
 	}
 	types.Money.ldbFormat = types.Money.tooltipFormat
 
-	local L = {
+	local locale = {
 		factionIconName = 'Faction icons',
 		factionIconUndecidedName = 'Undecided faction icon',
 
@@ -153,13 +114,39 @@ local function OpenConfiguration(self, args)
 		},
 	}
 
-	-- LibStub('LibDualSpec-1.0'):EnhanceDatabase(addon.db, addonName)
-	local AceConfig, AceConfigDialog = LibStub('AceConfig-3.0'), LibStub('AceConfigDialog-3.0')
-	local optionsTable = LibStub('LibOptionsGenerate-1.0'):GetOptionsTable(addon.db, types, L, true)
-	      optionsTable.name = addonName
+	-- variable, typeMappings, L, includeNamespaces, callback
+	return addon.db, types, locale, true, nil
+end
 
-	-- allow deleting characters
-	optionsTable.args.global.args.DeleteCharacter = {
+local function AddConfigurationExtras(optionsTable)
+	local characters = {}
+	local function GetDeletableCharacters()
+		local thisCharacter = addon.data.GetCurrentCharacter()
+		addon.data.GetAllCharacters(characters)
+		for i, characterKey in ipairs(characters) do
+			if characterKey ~= thisCharacter then
+				characters[characterKey] = ('%s: %s'):format(addon.data.GetRealm(characterKey), addon.data.GetCharacterText(characterKey))
+				-- local realmName = addon.data.GetRealm(characterKey)
+				-- characters[realmName] = characters[realmName] or {}
+				-- characters[realmName][characterKey] = addon.data.GetCharacterText(characterKey)
+			end
+			characters[i] = nil
+		end
+		return characters
+	end
+	StaticPopupDialogs['TWINKLE_DELETE_CHARACTER'] = {
+		text = 'Are you sure you want to delete |nall data of %s on %s?',
+		button1 = _G.OKAY,
+		button2 = _G.CANCEL,
+		OnAccept = function(_, characterKey) addon.data.DeleteCharacter(characterKey) end,
+		timeout = 0,
+		whileDead = true,
+		hideOnEscape = true,
+		preferredIndex = 3,
+	}
+
+	-- Allow deleting characters.
+	optionsTable.args.profile.args.DeleteCharacter = {
 		type = 'select',
 		name = 'Delete character',
 		desc = 'Select a character to be deleted. The logged in character cannot be deleted.|n'
@@ -176,41 +163,49 @@ local function OpenConfiguration(self, args)
 		end,
 		order = 99,
 	}
+end
 
+local function InitializeConfiguration(self, args)
+	local AceConfig = LibStub('AceConfig-3.0')
+
+	LibStub('LibDualSpec-1.0'):EnhanceDatabase(addon.db, addonName)
+
+	-- Initialize main panel.
+	local optionsTable = LibStub('LibOptionsGenerate-1.0'):GetOptionsTable(GetConfigurationVariables())
+	      optionsTable.name = addonName
+	if AddConfigurationExtras then AddConfigurationExtras(optionsTable) end
 	AceConfig:RegisterOptionsTable(addonName, optionsTable)
-	AceConfigDialog:AddToBlizOptions(addonName, nil, nil)
 
-	-- add entries for submodules
-	--[[ local AceConfigRegistry = LibStub('AceConfigRegistry-3.0')
+
+	-- Add panels for submodules.
+	local AceConfigRegistry = LibStub('AceConfigRegistry-3.0')
 	for name, subModule in addon:IterateModules() do
 		if AceConfigRegistry.tables[subModule.name] then
 			AceConfigDialog:AddToBlizOptions(subModule.name, name, addonName)
 		end
-		for subName, subSubModule in subModule:IterateModules() do
-			if AceConfigRegistry.tables[subSubModule.name] then
-				AceConfigDialog:AddToBlizOptions(subSubModule.name, subName, addonName)
-			end
-		end
-	end --]]
-	local optionsTable = LibStub('AceDBOptions-3.0'):GetOptionsTable(addon.db)
-	      optionsTable.name = addonName .. ' - ' .. optionsTable.name
-	AceConfig:RegisterOptionsTable(addonName..'_profiles', optionsTable)
-	AceConfigDialog:AddToBlizOptions(addonName..'_profiles', 'Profiles', addonName)
-
-	-- do all this only once. next time, only show this panel
-	OpenConfiguration = function(panel, args)
-		InterfaceOptionsFrame_OpenToCategory(addonName)
 	end
-	C_Timer.After(0.05, OpenConfiguration)
+
+	if addon.db.defaults and addon.db.defaults.profile and next(addon.db.defaults.profile) then
+		-- Add panel for profile settings.
+		local profileOptions = LibStub('AceDBOptions-3.0'):GetOptionsTable(addon.db)
+		profileOptions.name = addonName .. ' - ' .. profileOptions.name
+		AceConfig:RegisterOptionsTable(addonName..'_profiles', profileOptions)
+		AceConfigDialog:AddToBlizOptions(addonName..'_profiles', 'Profiles', addonName)
+	end
+
+	-- Restore original OnShow handler.
+	self:SetScript('OnShow', self.origOnShow)
+	self.origOnShow = nil
+
+	InterfaceAddOnsList_Update()
+	InterfaceOptionsList_DisplayPanel(self)
 end
 
--- create a fake configuration panel
-local panel = CreateFrame('Frame')
-      panel.name = addonName
-      panel:Hide()
-      panel:SetScript('OnShow', OpenConfiguration)
-InterfaceOptions_AddCategory(panel)
+-- Create a placeholder configuration panel.
+local panel = AceConfigDialog:AddToBlizOptions(addonName)
+panel.origOnShow = panel:GetScript('OnShow')
+panel:SetScript('OnShow', InitializeConfiguration)
 
 -- use slash command to toggle config
-_G['SLASH_'..addonName] = '/'..addonName
-_G.SlashCmdList[addonName] = function(args) OpenConfiguration(panel, args) end
+_G['SLASH_'..addonName..'1'] = '/'..addonName
+_G.SlashCmdList[addonName] = function(args) InterfaceOptionsFrame_OpenToCategory(addonName) end
