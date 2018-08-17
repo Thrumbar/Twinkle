40200b6c31ac6f4e89d114431d95edbe02250159
ckaotik <ckaotik@gmail.com>
2018-08-17 12:03:50 +0200

    Garrison: Made broker tooltip more generic, now also includes 8.0 garrison

--- brokers/garrison.lua
+++ brokers/garrison.lua
@@ -10,22 +10,31 @@ local emptyTable = {}
 local defaults = {
 	profile = {
 		cacheFullWarningPercent = 0.75,
-		currencies = {
-			[824] = true,
-			[1101] = true,
-			[1220] = true,
+		 followerTypes = {
+		 	-- Available types are extracted in :OnEnable.
+			-- [_G.LE_FOLLOWER_TYPE_GARRISON_6_0] = true,
+			-- [_G.LE_FOLLOWER_TYPE_SHIPYARD_6_2] = true,
+			-- [_G.LE_FOLLOWER_TYPE_GARRISON_7_0] = true,
+			-- [_G.LE_FOLLOWER_TYPE_GARRISON_8_0] = true,
 		},
-		--[[ followerTypes = {
-			[_G.LE_FOLLOWER_TYPE_GARRISON_6_0] = true,
-			[_G.LE_FOLLOWER_TYPE_SHIPYARD_6_2] = true,
-			[_G.LE_FOLLOWER_TYPE_GARRISON_7_0] = true,
-		}, --]]
 	},
 }
 
+local garrisonFollowerTypes = {}
+
 function broker:OnEnable()
+	-- Gather garrison type lookup data.
+	for garrisonFollowerType, _ in pairs(_G.GarrisonFollowerOptions) do
+		table.insert(garrisonFollowerTypes, garrisonFollowerType)
+
+		-- Enable by default.
+		defaults.profile.followerTypes[garrisonFollowerType] = true
+	end
+	table.sort(garrisonFollowerTypes)
+
 	self.db = addon.db:RegisterNamespace('Garrison', defaults)
 	-- self:RegisterEvent('EVENT_NAME', self.Update, self)
+
 	self:Update()
 end
 function broker:OnDisable()
@@ -33,7 +42,11 @@ function broker:OnDisable()
 end
 
 function broker:OnClick(btn, down)
-	-- do something, like show a UI
+	if btn == 'RightButton' then
+		InterfaceOptionsFrame_OpenToCategory(addonName)
+	else
+		GarrisonLandingPage_Toggle()
+	end
 end
 
 function broker:UpdateLDB()
@@ -47,21 +60,18 @@ local ZERO = _G.GRAY_FONT_COLOR_CODE .. '0|r'
 local COMPLETE_ICON = _G.GREEN_FONT_COLOR_CODE .. '%d|r|TInterface\\RAIDFRAME\\ReadyCheck-Ready:0|t '
 local ACTIVE_ICON = _G.NORMAL_FONT_COLOR_CODE .. '%d|r|TInterface\\FriendsFrame\\StatusIcon-Away:0|t '
 
-local layout, currencies, currencyCounts, missionCounts = {}, {}, {}, {}
+local layout, currencyCounts, missionCounts = {}, {}, {}
 function broker:UpdateTooltip()
 	local missionIcon = '|TInterface\\HELPFRAME\\OpenTicketIcon:18|t'
 	wipe(layout)
-	wipe(currencies)
-	table.insert(layout, 'LEFT')
-	for currencyID, enabled in pairs(broker.db.profile.currencies) do
-		if enabled then
-			table.insert(currencies, currencyID)
-			table.insert(layout, 'RIGHT')
-			table.insert(layout, 'LEFT')
+	table.insert(layout, 'LEFT') -- character
+	for i, garrisonFollowerType in ipairs(garrisonFollowerTypes) do
+		if broker.db.profile.followerTypes[garrisonFollowerType] then
+			table.insert(layout, 'RIGHT') -- currency
+			table.insert(layout, 'LEFT') -- missions
 		end
 	end
-	table.insert(layout, 'LEFT')
-	table.sort(currencies)
+	table.insert(layout, 'LEFT') -- work orders
 
 	local column = #layout
 	self:SetColumnLayout(column, unpack(layout))
@@ -71,15 +81,20 @@ function broker:UpdateTooltip()
 	lineNum = self:AddHeader(_G.CHARACTER)
 
 	column = 2
-	for _, currencyID in ipairs(currencies) do
+	for i, garrisonFollowerType in ipairs(garrisonFollowerTypes) do
+		-- Garrison types can have up to two associated currencies.
+		local primary, secondary = C_Garrison.GetCurrencyTypes(_G.GarrisonFollowerOptions[garrisonFollowerType].garrisonType)
+		local currencyID = garrisonFollowerType == _G.LE_FOLLOWER_TYPE_SHIPYARD_6_2 and secondary or primary
+
 		local currencyIcon = '|T'..(select(3, GetCurrencyInfo(currencyID)))..':0|t'
 		self:SetCell(lineNum, column, currencyIcon, 'RIGHT')
-		self.lines[lineNum].cells[column].link = 'currency:' .. currencyID
 		self:SetCellScript(lineNum, column, 'OnEnter', addon.ShowTooltip, self)
 		self:SetCellScript(lineNum, column, 'OnLeave', addon.HideTooltip, self)
+		self.lines[lineNum].cells[column].link = 'currency:' .. currencyID
 		column = column + 1
 
 		self:SetCell(lineNum, column, missionIcon, 'CENTER')
+		self.lines[lineNum].cells[column].tiptext = _G.GARRISON_MISSIONS
 		column = column + 1
 	end
 	self:SetCell(lineNum, column, _G.CAPACITANCE_WORK_ORDERS, 'LEFT')
@@ -87,7 +102,6 @@ function broker:UpdateTooltip()
 	self:AddSeparator(2)
 
 	addon.data.GetCharacters(characters)
-	-- table.sort(characters, Sort) -- TODO
 
 	local now, hasData = time(), false
 	local shipmentInterval = 4*60*60 -- every four hours
@@ -115,13 +129,6 @@ function broker:UpdateTooltip()
 			end
 		end
 
-		-- Reset statistics.
-		for followerType, info in pairs(missionCounts) do
-			info.active = 0
-			info.completed = 0
-			info.text = nil
-		end
-
 		-- builds
 		local numActive, numCompleted = 0, 0
 		for _, buildingID, rank, _, _, expires in DataStore:IteratePlots(characterKey) or nop do
@@ -143,6 +150,11 @@ function broker:UpdateTooltip()
 		end
 
 		-- missions
+		for followerType, info in pairs(missionCounts) do
+			info.active = 0
+			info.completed = 0
+			info.text = nil
+		end
 		for missionID, expires in pairs(DataStore:GetMissions(characterKey, 'active') or emptyTable) do
 			local followerType = DataStore:GetBasicMissionInfo(missionID)
 			if not missionCounts[followerType] then
@@ -155,6 +167,7 @@ function broker:UpdateTooltip()
 			end
 		end
 
+		local hasMissionData = false
 		for followerType, info in pairs(missionCounts) do
 			if info.completed and info.completed > 0 then
 				info.text = (info.text or '') .. COMPLETE_ICON:format(info.completed)
@@ -162,15 +175,24 @@ function broker:UpdateTooltip()
 			if info.active and info.active > 0 then
 				info.text = (info.text or '') .. ACTIVE_ICON:format(info.active)
 			end
+			hasMissionData = hasMissionData or (info.text and info.text ~= '')
 		end
-		local missions = missionCounts[_G.LE_FOLLOWER_TYPE_GARRISON_6_0] and missionCounts[_G.LE_FOLLOWER_TYPE_GARRISON_6_0].text or ''
-		local shipyardMissions = missionCounts[_G.LE_FOLLOWER_TYPE_SHIPYARD_6_2] and missionCounts[_G.LE_FOLLOWER_TYPE_SHIPYARD_6_2].text or ''
-		local orderHallMissions = missionCounts[_G.LE_FOLLOWER_TYPE_GARRISON_7_0] and missionCounts[_G.LE_FOLLOWER_TYPE_GARRISON_7_0].text or ''
 
 		-- resources
-		for i, currency in ipairs(currencies) do
-			currencyCounts[i] = select(3, addon.data.GetCurrencyInfo(characterKey, currency))
+		wipe(currencyCounts)
+		for i, garrisonFollowerType in ipairs(garrisonFollowerTypes) do
+			local currencyID, otherCurrencyID = C_Garrison.GetCurrencyTypes(_G.GarrisonFollowerOptions[garrisonFollowerType].garrisonType)
+
+			local count = select(3, addon.data.GetCurrencyInfo(characterKey, currencyID))
+			table.insert(currencyCounts, count)
+
+			if otherCurrencyID > 0 then
+				count = select(3, addon.data.GetCurrencyInfo(characterKey, otherCurrencyID))
+				table.insert(currencyCounts, count)
+			end
 		end
+		local hasResourceData = #currencyCounts > 0
+
 		local warning = ''
 		local numUncollected, cacheSize = DataStore:GetUncollectedResources(characterKey)
 		numUncollected, cacheSize = numUncollected or 0, cacheSize or 500
@@ -179,24 +201,39 @@ function broker:UpdateTooltip()
 		end
 
 		-- create tooltip row
-		if missions ~= '' or shipments ~= '' or orderHallMissions ~= ''
-			or (currencyCounts[1] + numUncollected) > 0 or currencyCounts[2] > 0 or currencyCounts[3] > 0 then
+		if hasMissionData or hasResourceData or shipments ~= '' then
 			local characterName = addon.data.GetCharacterText(characterKey)
-			lineNum = self:AddLine(characterName,
-				warning .. addon.ColorizeText(AbbreviateLargeNumbers(currencyCounts[1]), 10000 - currencyCounts[1], 10000),
-				missions,
-				_G.NORMAL_FONT_COLOR_CODE .. AbbreviateLargeNumbers(currencyCounts[2]) .. '|r',
-				shipyardMissions,
-				_G.NORMAL_FONT_COLOR_CODE .. AbbreviateLargeNumbers(currencyCounts[3]) .. '|r',
-				orderHallMissions,
-				shipments .. '\32\32'
-			)
+			lineNum = self:AddLine(characterName)
+
 			-- enable row highlight and tooltips
 			self:SetLineScript(lineNum, 'OnEnter', nop)
-			self.lines[lineNum].cells[2].tiptext = _G.GARRISON_RESOURCES_LOOT:format(numUncollected)
-			self:SetCellScript(lineNum, 2, 'OnEnter', addon.ShowTooltip, self)
-			self:SetCellScript(lineNum, 2, 'OnLeave', addon.HideTooltip, self)
 			hasData = true
+
+			column = 2
+			for i, garrisonFollowerType in ipairs(garrisonFollowerTypes) do
+				-- currency
+				local currencyText = AbbreviateLargeNumbers(currencyCounts[i])
+				if garrisonFollowerType == _G.LE_FOLLOWER_TYPE_GARRISON_6_0 then
+					currencyText = warning .. addon.ColorizeText(currencyText, 10000 - currencyCounts[i], 10000)
+					self:SetCell(lineNum, column, currencyText, 'RIGHT')
+
+					self.lines[lineNum].cells[column].tiptext = _G.GARRISON_RESOURCES_LOOT:format(numUncollected)
+					self:SetCellScript(lineNum, column, 'OnEnter', addon.ShowTooltip, self)
+					self:SetCellScript(lineNum, column, 'OnLeave', addon.HideTooltip, self)
+				else
+					currencyText = _G.NORMAL_FONT_COLOR_CODE .. currencyText .. '|r'
+					self:SetCell(lineNum, column, currencyText, 'RIGHT')
+				end
+				column = column + 1
+
+				-- missions
+				local missionText = missionCounts[garrisonFollowerType] and missionCounts[garrisonFollowerType].text or ''
+				self:SetCell(lineNum, column, missionText, 'LEFT')
+				column = column + 1
+			end
+
+			self:SetCell(lineNum, column, shipments .. '\32\32', 'LEFT')
+			column = column + 1
 		end
 	end
 
